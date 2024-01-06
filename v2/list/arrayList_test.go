73b871ea0729990c785221cd396b1b19aae8446f
schbm <mrcl.schubert@gmail.com>
2024-01-06 13:41:41 +0100

    fix

--- v2/list/arrayList_test.go
+++ v2/list/arrayList_test.go
@@ -7,167 +7,52 @@ import (
 
 func TestArrayList(t *testing.T) {
 	t.Log("Testing ArrayList")
-	list := NewArrayList[int]()
-	if list.Size() != 0 {
-		t.Error("list should be empty")
-	}
-	if !list.IsEmpty() {
-		t.Error("list should be empty")
+
+	tests := []struct {
+		name string
+		size int
+	}{
+		{
+			name: "100",
+			size: 100,
+		},
+		{
+			name: "1000",
+			size: 1000,
+		},
+		{
+			name: "10000",
+			size: 1000,
+		},
 	}
 
-	// test append 3 values
-	list.Append(1)
-	list.Append(2)
-	list.Append(3)
-	if list.Size() != 3 {
-		t.Error("list should have 3 elements")
-	}
-	if list.IsEmpty() {
-		t.Error("list should not be empty")
-	}
+	for _, test := range tests {
+		data := make([]int, test.size)
 
-	// test get first element
-	f, err := list.Get(0)
-	if err != nil {
-		t.Error(err)
-	}
-	if f != 1 {
-		t.Error("first element should be 1")
-	}
+		list := NewArrayList[int]()
 
-	// get middle element
-	m, err := list.Get(1)
-	if err != nil {
-		t.Error(err)
-	}
-	if m != 2 {
-		t.Error("middle element should be 2")
-	}
+		for i := 0; i < test.size; i++ {
+			data[i] = i
+		}
 
-	// get last element
-	l, err := list.Get(2)
-	if err != nil {
-		t.Error(err)
-	}
+		for _, number := range data {
+			list.Append(number)
+		}
 
-	if l != 3 {
-		t.Error("last element should be 3")
-	}
+		if list.Size() != test.size {
+			t.Error("incorrect size")
+		}
 
-	// test insert first middle last
-	err = list.Insert(0, 0)
-	if err != nil {
-		t.Error(err)
-	}
-	err = list.Insert(4, 1)
-	if err != nil {
-		t.Error(err)
-	}
-	err = list.Insert(5, 2)
-	if err != nil {
-		t.Error(err)
-	}
-
-	v, err := list.Get(1)
-	if err != nil {
-		t.Error(err)
-	}
-	if v != 4 {
-		t.Error("middle element should be 4")
-	}
-
-	//test remove first middle last
-	err = list.Remove(0)
-	if err != nil {
-		t.Error(err)
-	}
-	err = list.Remove(1)
-	if err != nil {
-		t.Error(err)
-	}
-	err = list.Remove(2)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if list.Size() != 3 {
-		t.Error("list should have 3 elements")
-	}
-
-	//test out of bounds
-	err = list.Remove(3)
-	if err == nil {
-		t.Error("should have gotten an error")
-	}
-
-	// test out of bound insert
-	err = list.Insert(0, 4)
-	if err == nil {
-		t.Error("should have gotten an error")
+		val, err := list.Get(test.size - 1)
+		if err != nil || val != test.size-1 {
+			t.Errorf("max number cannot be found")
+		}
 	}
 }
 
-func TestArrayListTime(t *testing.T) {
-	t.Log("Testing ArrayList Time")
-	currT := time.Now()
-	list := NewArrayList[int]()
-	for i := 0; i < 10000000; i++ {
-		list.Append(i)
+func MeasureTime(name string, t *testing.T) func() {
+	start := time.Now()
+	return func() {
+		t.Logf("Time taken by %s is %v\n", name, time.Since(start))
 	}
-	t.Log("appended 10000000 elements in: ", time.Since(currT))
-
-	//get the last element
-	currT = time.Now()
-	_, _ = list.Get(list.Size() - 1)
-	t.Log("ArrayList Get the last element: ", time.Since(currT))
-
-	//get middle
-	currT = time.Now()
-	_, _ = list.Get(list.Size() / 2)
-	t.Log("ArrayList Get the middle element: ", time.Since(currT))
-
-	//insert first
-	currT = time.Now()
-	_ = list.Insert(1, 0)
-	t.Log("ArrayList Insert first element: ", time.Since(currT))
-
-	//insert last
-	currT = time.Now()
-	_ = list.Insert(1, list.Size()-1)
-	t.Log("ArrayList Insert last element: ", time.Since(currT))
-
-	//insert middle
-	currT = time.Now()
-	_ = list.Insert(1, list.Size()/2)
-	t.Log("ArrayList Insert middle element: ", time.Since(currT))
-
-	//remove first
-	currT = time.Now()
-	_ = list.Remove(0)
-	t.Log("ArrayList Remove first element: ", time.Since(currT))
-
-	//remove last
-	currT = time.Now()
-	_ = list.Remove(list.Size() - 1)
-	t.Log("ArrayList Remove last element: ", time.Since(currT))
-
-	//remove middle
-	currT = time.Now()
-	_ = list.Remove(list.Size() / 2)
-	t.Log("ArrayList Remove middle element: ", time.Since(currT))
-
-	//remove high int value
-	currT = time.Now()
-	_ = list.Remove(list.Size() - 100)
-	t.Log("ArrayList Remove high int value element: ", time.Since(currT))
-
-	//remove high int value
-	currT = time.Now()
-	_ = list.Remove(list.Size() - 200)
-	t.Log("ArrayList Remove high int value element: ", time.Since(currT))
-
-	//remove high int value
-	currT = time.Now()
-	_ = list.Remove(list.Size() - 300)
-	t.Log("ArrayList Remove high int value element: ", time.Since(currT))
 }
